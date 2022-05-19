e21bb1f3d0417404a05292930681d37e9af02d35
J <guji.super@gmail.com>
2022-05-19 18:20:11 +0800

    optimize

--- RemoteSpyPlus.lua
+++ RemoteSpyPlus.lua
@@ -170,8 +170,7 @@ local function CanSpy(fName)
 	end
 
 	for k,v in pairs(shared.NoSpyList) do
-		print("Can Spy " .. tostring(fName) .. " " .. v)
-		if string.find(fName, v) > 0 then
+		if string.find(fName, v) then
 			return false
 		end
 	end
